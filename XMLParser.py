a7b507ae3b894ef1f5e1995f882c98fdff93b8dd
Joaquín Jiménez <joaquinenrique2010@gmail.com>
2021-09-01 11:09:20 -0600

    U: word count

--- XMLParser.py
+++ XMLParser.py
@@ -15,34 +15,62 @@ class XMLParser:
     def removeTags(self, text):
         return re.sub(pattern="\<[^>]*\>", repl=" " , string=text)
 
-    # removes tags
     # removes accents
     # convert to lowercase
     def normalize(self, text):
-        onlyText = self.removeTags(text)
         accepted_accents = {u'\N{COMBINING TILDE}',}
-        return ''.join(c for c in unicodedata.normalize('NFKD', onlyText) 
+        return ''.join(c for c in unicodedata.normalize('NFKD', text) 
                         if unicodedata.category(c) != 'Mn' or c in accepted_accents).lower()    
 
 
     def splitToAcceptedWords(self,text):
-        # filter out empty words
-        return [word for word in re.split("\W+", text) if word not in self.stopwords]  
+        text = self.removeTags(text)
+        allWords = self.normalize(re.sub(pattern="[\W]", repl=" " ,string=text)).split()
+        acceptedWords = []
+        for word in allWords:
+            #word = unicode(word,"utf-8")
+            if word not in self.stopwords:
+                acceptedWords += [word]
+        return acceptedWords
+
+    def process(self, text):
+        #To lowercase
+        text = text.lower()
+        # Remove accents
+        trans_tab = dict.fromkeys(map(ord, u'\u0301\u0308'), None)
+        text = unicodedata.normalize('NFKC', unicodedata.normalize('NFKD', text).translate(trans_tab))
+        #Find valid words
+        tokens = re.findall("[a-zA-Z0-9ñ]+",text)
+        #Sort the words  
+        tokens = sorted(tokens)
+
+        acceptedWords = []
+        for word in tokens:
+            if word not in self.stopwords:
+                acceptedWords += [word]
+
+        #Group words
+        return dict(Counter(acceptedWords))
 
     def wordCount(self, fname):
         try:
             with open(fname, encoding='utf-8') as f:
-                    normalizedText = self.normalize(f.read())
-                    return Counter(self.splitToAcceptedWords(normalizedText))
+                    return self.process(self.removeTags(f.read()))
         except:
             #if not utf-8 text file
             with open(fname) as f:
-                    normalizedText = self.normalize(f.read())
-                    return Counter(self.splitToAcceptedWords(normalizedText))   
+                    return self.process(self.removeTags(f.read()))  
 
     def wordCountText(self, text, stopwords):
         self.stopwords = stopwords
-        normalizedText = self.normalize(text)
-        return Counter(self.splitToAcceptedWords(normalizedText))
-#parser = XMLParser(["para","mas","el","sus","al","de"])
+        return self.process(text)
+
+#parser = XMLParser()
+#parser.setStopwords(['a', 'ante', 'bajo', 'cabe', 'con', 'contra', 'de', 'desde', 'e', 'el', 'en', 'entre', 'hacia', 'hasta', 'ni', 'la', 'le', 'lo', 'los', 'las', 'o', 'para', 'pero', 'por', 'que', 'se', 'segun', 'sin', 'so', 'sobre', 'tras', 'u', 'un', 'una', 'unas', 'uno', 'unos', 'y'])
+#print(parser.wordCount(r'D:\joaqu\Documents\GitHub\RIT_TP1\xml-es\xacc-y2k.xml'))
+#with open(r'D:\joaqu\Documents\GitHub\RIT_TP1\xml-es\xacc-y2k.xml',encoding='utf-8') as f:
+#    for word in parser.splitToAcceptedWords(f.read()):
+#        print(word)
+
+#print(parser.wordCount(r'D:\joaqu\Documents\GitHub\RIT_TP1\xml-es\xacc-y2k.xml'))
 #print(parser.wordCount(r"D:\joaqu\Documents\GitHub\RIT_TP1\xml-es\apx-authors.xml"))
